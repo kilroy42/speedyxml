ab0daa7c1e78fa2f5d112c6d92af94a2d8b7e302
kilroy <kilroy@rz-online.de>
2025-10-22 14:11:40 +0200

    add .pyi file, again

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README", "r", encoding="utf-8") as fh:
 
 setup(
 	name				=	'speedyxml',
-	version				=	'0.4.0.3',
+	version				=	'0.4.0.4',
 	description			=	'Speedy XML parser for Python',
 	author				=	'kilroy',
 	author_email		=	'kilroy@81818.de',
@@ -18,7 +18,7 @@ setup(
 	ext_modules			=	[
 		Extension('speedyxml', ['src/speedyxml.c'])
 	],
-    package_data        =   {'': ['speedyxml.pyi']},
+    data_files          =   [('.', ['speedyxml.pyi'])],
 	test_suite			=	'test.test.suite',
 	classifiers			=	[
 		'Development Status :: 4 - Beta',
